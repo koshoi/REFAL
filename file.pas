dfbc8433dd27d735c798b7d8c3e8b1d3cb653c28
Oleg Koshovets <okoshovetc@gmail.com>
2017-02-12 00:14:50 +0300

    it is only translation now

--- file.pas
+++ file.pas
@@ -9,9 +9,15 @@ procedure proc1(var aa: integer)
 var
 begin
 	writeln(aa);
+	aa:=i;
+end;
+function func1(var aa: integer) : integer
+var
+begin
+	aa:=b;
 end;
 begin
-	for i:=1 to 10 do aa:=b;
-	i:=1;
-	proc1(a);
+	for i:=1 to 10 do proc1(aa);
+	i:=func1(aa);
+	proc1(aa);
 end.
